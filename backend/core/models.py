61941150f22a1458d8d29136aad7667689ba1641
pcuestamorer <pablocuestam@outlook.com>
2023-07-31 12:54:24 +0200

    post insert a contact

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django_extensions.db.models import (
+    TimeStampedModel,
+    ActivatorModel,
+    TitleDescriptionModel,
+)
 
-# Create your models here.
+
+class Contact(
+    TimeStampedModel,
+    ActivatorModel,
+    TitleDescriptionModel,
+    models.Model,
+):
+    class Meta:
+        verbose_name_plural = "Contacts"
+
+    email = models.EmailField(verbose_name="Email")
+
+    def __str__(self):
+        return f"{self.title}"
--- backend/drf/urls.py
+++ backend/drf/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
+from core import views as core_views
 
 router = routers.DefaultRouter()
 
@@ -23,4 +24,5 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view()),
 ]
